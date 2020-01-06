dfc91f2692f5fbbced995f476a55f5b6314755bb
rp722 <55685889+rp722@users.noreply.github.com>
2020-01-06 21:37:55 +0800

    Create createTB.sql

---
+++ createTB.sql
@@ -0,0 +1,9 @@
+CREATE TABLE [dbo].[leftUser](
+    [lid] [int] NULL,                      ---i
+    [wid] [varchar](3) NULL,        --工号
+    [workStatus] [bit] NULL,        --是否离职
+    [aduser] [nvarchar](5) NULL     --ad账号
+) ON [PRIMARY]
+GO
+
+

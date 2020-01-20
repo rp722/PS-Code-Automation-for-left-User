fadf92959401b51456c34d349a44a3c7811d68b2
rp722 <55685889+rp722@users.noreply.github.com>
2020-01-20 21:57:55 +0800

    Update createTB.sql

--- createTB.sql
+++ createTB.sql
@@ -1,8 +1,8 @@
 CREATE TABLE [dbo].[leftUser](
-    [lid] [int] NULL,                      ---i
-    [wid] [varchar](3) NULL,        --工号
-    [workStatus] [bit] NULL,        --是否离职
-    [aduser] [nvarchar](5) NULL     --ad账号
+    [lid] [int] NULL,                      ---id
+    [wid] [varchar](3) NULL,        --工号   workid
+    [workStatus] [bit] NULL,        --是否离职  leave or not
+    [aduser] [nvarchar](5) NULL     --ad账号   AD accout
 ) ON [PRIMARY]
 GO
 
